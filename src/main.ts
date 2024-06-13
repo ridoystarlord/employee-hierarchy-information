29694f90e77df945693b3ec6a1d9dd2f051efb2d
Ridoy <ridoy.starlord@gmail.com>
2024-06-14 00:35:46 +0600

    feat: added cluster module for scale the application

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,6 @@
+import * as notReallyCluster from 'cluster';
+import { availableParallelism } from 'os';
+
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory, HttpAdapterHost } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
@@ -10,13 +13,17 @@ import { AppModule } from './app.module';
 import corsOptions from './utils/cors';
 import Logger from './utils/logger';
 
+const cluster = notReallyCluster as unknown as notReallyCluster.Cluster;
+const numCPUs = availableParallelism();
+
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: false,
+  });
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new AllExceptionsFilter(httpAdapter));
   app.enableCors(corsOptions);
   app.use(helmet());
-  // app.use(morgan('tiny'));
   const morganFormat = ':method :url :status :response-time ms';
   app.use(
     morgan(morganFormat, {
@@ -44,6 +51,21 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api-docs', app, document);
+
   await app.listen(process.env.PORT);
 }
-bootstrap();
+
+if (cluster.isPrimary) {
+  // Fork workers.
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker) => {
+    console.log(`worker ${worker.process.pid} died`);
+    cluster.fork(); //fork new worker
+  });
+} else {
+  bootstrap();
+  console.log(`Worker ${process.pid} started`);
+}
